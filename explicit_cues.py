da6930d2e4a27adffcbc8205d950c0e1dac1ea8d
Kyra Wilson <wilsonk2@carleton.edu>
2020-01-12 13:12:22 -0600

    update concepts.rmd

--- explicit_cues.py
+++ explicit_cues.py
@@ -7,6 +7,10 @@ How to run: explicit_cues.py followed by ages (<3, <4, <5, or <6) you want to ge
 Ex: "explicit_cues.py 3 4" will get co-occurrences for all utterances for children under 3 and all utterances for children under 4
 
 TODO:
+*make total number of occurrences (caculated in utterances_to_txt) a csv instead of just print statement
+*make a cleaner file structure
+*make it run faster by tacking on ages instead of researching each time
+
 add in code to get broad concepts (if necessary)
 test & make sure output is what we actually want...
 
@@ -15,6 +19,161 @@ test & make sure output is what we actually want...
 import csv
 import re
 import sys
+
+'''-----------------------------------------------------------------------------------------------------'''
+'''THIS CODE IS AWFUL AND BAD AND NEEDS TO BE FIXED SOON'''
+n = 0
+all = True
+
+'''
+PARAMETERS: n, the size of the window to search
+            all, whether to search for co-occurrence in all categories or only the correct superordinate category.
+Find any co-ocurrences of basic terms and superordinate categroies in utterances from childes database.
+'''
+def concept_and_instance(n, all):
+    conceptLists = ["food", "clothes", "animals", "toys", "furniture"]
+
+    if n == 0:
+        concept_and_instance_zero(conceptLists, all)
+
+    else:
+        n = 2 * n + 1
+
+        curUtterances = [""] * n
+
+        with open('all_utterances_3.csv', newline='') as f:
+            reader = csv.reader(f)
+
+            with open('concept_and_instance_all_verbs_n=0_3.csv', 'w') as writeFile:
+                writer = csv.writer(writeFile)
+
+                for i in range(n):
+                    row = next(reader)
+                    curUtterances[i] = [row[8], row[12], row[14], row[16], row[25]]
+
+                for i in range(0, n//2):
+                    concept_and_instance_helper(i, curUtterances, conceptLists, writer, all)
+
+                for row in reader:
+                    i = 2
+                    concept_and_instance_helper(i, curUtterances, conceptLists, writer, all)
+                    curUtterances.pop(0)
+                    curUtterances.append([row[8], row[12], row[14], row[16], row[25]])
+
+                for i in range(n // 2 + 1, n):
+                    concept_and_instance_helper(i, curUtterances, conceptLists, writer, all)
+
+
+def concept_and_instance_helper(i, curUtterances, conceptLists, writer, all):
+    if all:
+        for concept in conceptsDict.keys():
+            if concept in curUtterances[i][0].split():
+                temp = []
+                temp.append(conceptsDict[concept])
+                temp.append(" ")
+                temp.append((curUtterances[i][0], curUtterances[i][1]))
+
+                for utt in curUtterances:
+                    for group in conceptLists:
+                        for instance in data[group]:
+                            if instance in utt[0].split() and instance != concept:
+                                temp.append([curUtterances.index(utt), utt])
+                                temp.append(instance)
+                                temp.append(curUtterances[i][2])
+                                temp.append(curUtterances[i][3])
+                                temp.append(curUtterances[i][4])
+                                if "Child" in curUtterances[i][1] and "Child" in utt[1]:
+                                    temp[1] = "C -> C"
+                                elif "Child" not in curUtterances[i][1] and "Child" not in utt[1]:
+                                    temp[1] = "A -> A"
+                                elif "Child" in curUtterances[i][1]:
+                                    temp[1] = "C -> A"
+                                elif "Child" in utt[1]:
+                                    temp[1] = "A -> C"
+                                writer.writerow(temp)
+                                temp = temp[:3]
+
+    #add something in here to know if utterance was a category utterance or verb utterance?
+    else:
+        for concept in conceptsDict.keys():
+            if concept in curUtterances[i][0].split():
+                temp = []
+                temp.append(conceptsDict[concept])
+                temp.append(" ")
+                temp.append((curUtterances[i][0], curUtterances[i][1]))
+
+                for utt in curUtterances:
+                    for instance in data[concept]:
+                        if instance in utt[0].split() and instance != concept:
+                            temp.append([curUtterances.index(utt), utt])
+                            temp.append(instance)
+                            temp.append(curUtterances[i][2])
+                            temp.append(curUtterances[i][3])
+                            temp.append(curUtterances[i][4])
+                            if "Child" in curUtterances[i][1] and "Child" in utt[1]:
+                                temp[1] = "C -> C"
+                            elif "Child" not in curUtterances[i][1] and "Child" not in utt[1]:
+                                temp[1] = "A -> A"
+                            elif "Child" in curUtterances[i][1]:
+                                temp[1] = "C -> A"
+                            elif "Child" in utt[1]:
+                                temp[1] = "A -> C"
+                            writer.writerow(temp)
+                            temp = temp[:3]
+
+#row[8] if 3,6 and row[x+1] if 4,5
+def concept_and_instance_zero(conceptLists, all):
+    with open('all_utterances_3.csv', newline='') as f:
+        reader = csv.reader(f)
+
+        with open('concept_and_instance_all_verbs_n=0_3.csv', 'w') as writeFile:
+            writer = csv.writer(writeFile)
+
+            if all:
+                for row in reader:
+                    for concept in conceptsDict.keys():
+                        if concept in row[8].split():
+                            temp = []
+                            temp.append(conceptsDict[concept])
+                            temp.append(" ")
+                            temp.append((row[8], row[12]))
+
+                            for group in conceptLists:
+                                for instance in data[group]:
+                                    if instance in row[8].split() and instance != concept:
+                                        temp.append([0, row[8]])
+                                        temp.append(instance)
+                                        temp.append(row[14])
+                                        temp.append(row[16])
+                                        temp.append(row[25])
+
+                                        writer.writerow(temp)
+
+                                        temp = temp[:3]
+            else:
+                for row in reader:
+                    for concept in concepts:
+                        if concept in row[8].split() or concept == "food" and "eat" in row[8].split() \
+                            or concept == "clothes" and "wear" in row[8].split() \
+                                or concept == "toy" and "play" in row[8].split():
+                            temp = []
+                            temp.append(concept)
+                            temp.append(" ")
+                            temp.append((row[8], row[12]))
+
+                            for instance in data[concept]:
+                                if instance in row[8].split() and instance != concept:
+                                    temp.append([0, row[8]])
+                                    temp.append(instance)
+                                    temp.append(row[14])
+                                    temp.append(row[16])
+                                    temp.append(row[25])
+
+                                    writer.writerow(temp)
+
+                                    temp = temp[:3]
+'''-----------------------------------------------------------------------------------------------------'''
+
 
 conceptsDict = {"animal": "animal", "eat": "food", "food": "food", "wear": "clothes", "clothing": "clothes",
                 "clothes": "clothes", "play": "toy", "toy": "toy", "play with": "toy", "furniture": "furniture",
@@ -41,7 +200,7 @@ def get_instances():
         for row in reader:
             # data[row[1]] = [x for x in row[2:] if x != "" and x != "animal" and x != "toy" and x != "food"]
 
-            if row[1] in broad_concept_categories:
+            if row[1] in concepts:
                 temp.append([x for x in row[2:] if x != "" and x != "animal" and x != "toy" and x != "food"])
 
         instances = [item for sublist in temp for item in sublist]
@@ -51,47 +210,57 @@ def get_instances():
 
 
 def utterances_to_txt(readfile, write_txt_file):
+    total_categories = {"animal": 0, "toy": 0, "vehicle": 0, "clothes": 0, "furniture": 0, "food": 0}
     with open(readfile, newline='') as f:
         reader = csv.reader(f)
 
         with open(write_txt_file, 'w') as writeFile:
-
             next(reader)
 
             for row in reader:
                 utt = row[8]
-                for word in broad_concept_categories:
+                for word in concepts:
                     if word in utt:
                         utt = re.sub(word, "", utt)
+                        total_categories[word] += 1
                 writeFile.write(utt + " ")
-                
+    
+    print(total_categories)
     f.close()
     writeFile.close()
     
 # Get co-occurrence of concepts and instances
-def get_broad_concepts(instances, readfile, write_broad_concept_file):
+# Output = concept_and_instance_all_age.csv
+def get_concept_cooccurrence(instances, readfile, write_broad_concept_file):
     with open(readfile, newline='') as f:
         reader = csv.reader(f)
 
-        with open(write_txt_file, 'w') as writeFile:
-            for instance in instances:
-                if instance in utt:
-                    for concept in broad_concepts:
-                        if concept in utt:
-                            temp = []
-                            temp.append(concept)
-                            temp.append(" ")
-                            temp.append((row[8], row[12]))
-                            temp.append([0, row[8]])
-                            temp.append(instance)
-                            temp.append(row[14])
-                            temp.append(row[16])
-                            temp.append(row[25])
+        with open(write_broad_concept_file, 'w') as writeFile:
+            writer = csv.writer(writeFile)
+            
+            for row in reader:
+                utt = row[8]
+                for instance in instances:
+                    if instance in utt:
+                        for concept in concepts:
+                            if concept in utt:
+                                temp = []
+                                temp.append(concept)
+                                temp.append(" ")
+                                temp.append((row[8], row[12]))
+                                temp.append([0, row[8]])
+                                temp.append(instance)
+                                temp.append(row[14])
+                                temp.append(row[16])
+                                temp.append(row[25])
 
-                            writer.writerow(temp)
+                                writer.writerow(temp)
+    f.close()
+    writeFile.close()
 
 #  Get categorization from verb+compl pattern
-def get_concepts(instances, readfile, write_concept_file):
+#  Output = broad_concepts_and_instances_age.csv
+def get_concept_verbs(instances, readfile, write_concept_file):
     count = 0
     
     with open(readfile, newline='') as f:
@@ -105,8 +274,8 @@ def get_concepts(instances, readfile, write_concept_file):
 
                 for instance in instances:
                      if instance in utt:
-                         for verb in verbs:
-                             if verb in row[8]:
+                        for verb in verbs:
+                            if verb in row[8]:
                                 regex = verb + r' ([a-z]* ){0,1}' + instance
                                 if re.search(regex, row[8]) is not None:
                                     temp = []
@@ -171,11 +340,11 @@ def main():
         print("Getting text for age < " + age + "...")
         utterances_to_txt(readfile, write_txt_file)
         
-        print("Getting co-occurrences for age < " + age + "...")
-        get_concepts(instances, readfile, write_concept_file)
+        print("Getting verb+compl for age < " + age + "...")
+        get_concept_verbs(instances, readfile, write_concept_file)
         
-        print("Getting broad concept co-occurrences for age < " + age + "...")
-        get_broad_concepts(instances, readfile, write_broad_concept_file)
+        print("Getting concept co-occurrences for age < " + age + "...")
+        get_concept_cooccurrence(instances, readfile, write_broad_concept_file)
         
         print("Done with age < " + age)
 
